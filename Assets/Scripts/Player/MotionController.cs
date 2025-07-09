c70d2a312fd978b96fba31f1614cac99fa72bda0
BeansMage42 <jonah.gibson0201@gmail.com>
2025-07-09 11:57:39 -0400

    hand tracking

---
+++ Assets/Scripts/Player/MotionController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionController : MonoBehaviour
+{
+    //ZED ASSETS
+    [SerializeField] private ZEDBodyTrackingManager btm;
+    [SerializeField] private ZEDManager zManager;
+
+    //POSITIONS
+    private Vector3 handLeftpos, handRightpos, prevLeftPos, prevRightPos;
+
+    //VISUALIZATION GAME OBJECTS
+    [SerializeField] GameObject leftHandGM, rightHandGM;
+    private Transform leftTransform, rightTransform;
+
+    [SerializeField] private float zDist = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zManager.OnBodyTracking += OnTrackHands;
+        leftTransform = leftHandGM.transform;
+        rightTransform = rightHandGM.transform;
+
+    }
+
+    private void OnTrackHands(BodyTrackingFrame bodyTrackFrame)
+    {
+        if (bodyTrackFrame.bodyCount > 0)
+        {
+            //left hand = point 8, right hand = point 15
+            
+
+            //create vector from last from to current frame and scale by screen size
+            //this gives the player a full range of motion on the screen space
+            handLeftpos = prevLeftPos - bodyTrackFrame.detectedBodies[0].rawBodyData.keypoint[8] * Camera.main.orthographicSize;
+            handRightpos = prevRightPos - bodyTrackFrame.detectedBodies[0].rawBodyData.keypoint[15] * Camera.main.orthographicSize;
+
+            //store current frame for the next loop
+            prevLeftPos = bodyTrackFrame.detectedBodies[0].rawBodyData.keypoint[8];
+            prevRightPos = bodyTrackFrame.detectedBodies[0].rawBodyData.keypoint[15];
+            
+            //set distance from camera
+            handLeftpos.z = zDist;
+            handRightpos.z = zDist;
+            //fix y reversal issue
+            handLeftpos.y *= -1;
+            handRightpos.y *= -1;
+
+            print("right hand at: " + handRightpos + " left hand at: " + handLeftpos);
+
+            //set positions on transform
+            leftTransform.position = handLeftpos;
+            rightTransform.position = handRightpos;
+        }
+    }
+}
